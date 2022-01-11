4e4bd3e4e7701e3de6ac4c576779f180c6fb150a
Alex <bara_x@mail.ru>
2022-01-11 18:54:52 +0300

    добавление картинок и сосздание классов и адаптора

---
+++ app/src/main/java/com/bara_x/myapplication/Plants.kt
@@ -0,0 +1,4 @@
+package com.bara_x.myapplication
+
+data class Plants(val imageId: Int, val title: String)
+
---
+++ app/src/main/java/com/bara_x/myapplication/model/PlantAdapter.kt
@@ -0,0 +1,12 @@
+package com.bara_x.myapplication.model
+
+import androidx.recyclerview.widget.RecyclerView
+
+class PlantAdapter: RecyclerView.Adapter<PlantAdapter.PlantHolder> {
+
+    class PlantHolder {
+
+    }
+
+
+}
